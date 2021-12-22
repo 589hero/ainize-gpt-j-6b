07631d03820346fb5c289bbe5746e35bea41c438
baek-donghoon <baek@comcom.ai>
2021-12-22 14:06:14 +0900

    remove miss parenthesis.

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 print("model loading...")
 
-# # Model & Tokenizer loading
-tokenizer = AutoTokenizer.from_pretrained("./model")
-model = AutoModelForCausalLM.from_pretrained("./model", torch_dtype=torch.float16)
+# # # Model & Tokenizer loading
+# tokenizer = AutoTokenizer.from_pretrained("./model")
+# model = AutoModelForCausalLM.from_pretrained("./model", torch_dtype=torch.float16)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# model.to(device)
 
 requests_queue = Queue()    # request queue.
 BATCH_SIZE = 1              # max request size.
@@ -46,6 +46,7 @@ handler = Thread(target=handle_requests_by_batch).start()
 
 
 def make_text(request_input: List) -> Dict:
+    return "result"
     try:
         text, max_length, temperature, top_p, repetition_penalty = request_input[0], request_input[1], request_input[2], request_input[3], request_input[4]
 
@@ -80,7 +81,7 @@ def generate():
     try:
         args = []
         text = request.form['text']
-        length = int(request.form.get('length'), 150)
+        length = int(request.form.get('length', 150))
         temperature = float(request.form.get('temperature', 0.9))
         top_p = float(request.form.get('top_p', 0.95))
         repetition_penalty = float(request.form.get('repetition_penalty', 0.8))
@@ -91,7 +92,10 @@ def generate():
         args.append(top_p)
         args.append(repetition_penalty)
 
+        print(args)
+
     except Exception as e:
+        print(e)
         return jsonify({'Error': 'Invalid request'}), 500
 
     req = {'input': args}
@@ -110,7 +114,8 @@ def health_check():
 
 @app.route('/')
 def main():
-    return "Hello", 200
+    # return render_template('main.html'), 200
+    return 'hello', 200
 
 
 if __name__ == '__main__':
